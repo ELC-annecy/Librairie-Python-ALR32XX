81e0196e8c56f4503d315ea7bc058243fdad4677
John-Yvan <moubajohnyvan@gmail.com>
2021-09-21 10:58:53 +0200

    Exemple 3

---
+++ Exemples/Exemple_3.py
@@ -0,0 +1,20 @@
+"""
+    ************************************************************************
+    *  fichier  : ALR32XX.py                                               *
+    *  Fonction : Classe principale                                        *
+    *  Produit  : ALR32XX                                                  *
+    *  Device   :                                                          *
+    *                                                                      *
+    *  Copyright     : ELC, tous droits reservés                           *
+    *  Auteur        : JY MOUBA                                            *
+    *  Date creation : 01 aout 2021                                        *
+    *  Version MAJ   : 01                                                  *
+    *                                                                      *
+    *  Historique :                                                        *
+    *                                                                      *
+    *  Version     Date       Auteur         Objet                         *
+    *  ------------------------------------------------------------------- *
+    *    1.0    21/09/2021    Y.M     Édition originale                    *
+    ************************************************************************
+"""
+
