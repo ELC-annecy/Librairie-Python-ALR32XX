dbe0fc8463666b17d1a97700092612f962ddcef8
John-Yvan <moubajohnyvan@gmail.com>
2021-09-29 09:25:11 +0200

    Modification programme ALR32XX 3

--- Setup.py
+++ Setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Library_ALR32XX",
-    version="0.0.1",
+    version="0.0.2",
     author="elc-construction-electronique",
     author_email="be@elc.fr",
     description="Ce module permet de commander via liaison série les alimentations programmables ALR32XX de ELC",
@@ -20,7 +20,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
+    py_modules=["Library_ALR32XX"]
+    package_dir={" ": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
